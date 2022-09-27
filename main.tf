dfa62155560198fcd994252389480c307e932ec6
Denis Santos <denis.santos@brambles.com>
2022-09-27 10:51:25 -0700

    initial commit

---
+++ main.tf
@@ -0,0 +1,38 @@
+data "subnet" "target_subnet" {
+  name = lower("tf-knowledge-share-vpc-${var.target_subnet}-${var.region}")
+}
+
+data "aws_vpc" "main_vpc" {
+  name = "tf-knowledge-share-vpc"
+}
+
+resource "aws_network_interface" "eth1" {
+  subnet_id   = data.subnet.target_subnet.id
+  tags = {
+    Name = "primary_network_interface"
+  }
+}
+
+resource "aws_instance" "instance" {
+  ami           = var.ami
+  instance_type = coalesce(var.instance_type, var.instance_type_conditional[lower(var.env)])
+
+  network_interface {
+    network_interface_id = aws_network_interface.eth1.id
+    device_index         = 0
+  }
+
+  root_block_device {
+    volume_size = var.vol_size
+    volume_type = var.vol_type == null ? var.vol_type_conditional[lower(var.env)] : var.vol_type
+  }
+
+user_data = templatefile("${path.module}/${var.user_data}", { "name" = "test"})
+tags = merge({Name = var.name},var.ec2_tags)
+
+}
+
+output "instance_id" {
+  description = "ID of instance"
+  value       = aws_instance.instance.id
+}
---
+++ variables.tf
@@ -0,0 +1,55 @@
+variable "vol_size" {
+  default = null
+}
+
+variable "vol_type" {
+  default = null
+}
+
+variable "ec2_tags" {
+  type = map(any)
+  default = {
+    "Name" = "ec2-module"
+  }
+}
+
+variable "instance_type" {
+  default = null
+}
+
+variable "ami" {
+  default = "ami-05fa00d4c63e32376"
+}
+
+variable "name" {
+  
+}
+
+variable "user_data" {
+  default = "userdata.sh.tpl"
+}
+
+variable "env" {
+  default = null
+}
+
+variable "instance_type_conditional" {
+  type = map
+  default = {
+    dev = "t2.micro"
+    prod = "t2.large"
+  }
+}
+
+variable "vol_type_conditional" {
+  type = map
+  default = {
+    dev = "gp2"
+    prod = "gp3"
+  }
+}
+
+variable "target_subnet" {
+  description = "Subnet to deploy EC2 values acceptable are"
+  default = null
+}
